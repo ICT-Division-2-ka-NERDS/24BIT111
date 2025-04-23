a5808d3b627ba9a467834d43b6fe3ca583311021
selvi0931 <24bit111@sot.pdpu.ac.in>
2025-04-23 12:33:26 +0530

    Add files via upload

---
+++ lab 1/22.py
@@ -0,0 +1,4 @@
+gross_sales = 5000
+discount = gross_sales * 0.10
+net_sales = gross_sales - discount
+print("Net Sales:", net_sales)
---
+++ lab 1/23.py
@@ -0,0 +1,7 @@
+sub1 = 80
+sub2 = 75
+sub3 = 90
+total = sub1 + sub2 + sub3
+average = total / 3
+print("Total:", total)
+print("Average:", average)
